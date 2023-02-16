bccb551dffc2bf0f1896fb7f85083c6227e19b09
Alex <xrlogic1@gmail.com>
2023-02-16 16:53:59 +0400

    *

---
+++ SwitchCaseStatement/SwitchCaseStatement.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main()
+{
+	// 3.14
+	// 3
+	// bool
+
+	// char - 1
+	// int - 2 or 4
+	// short - 2 
+	// long - 4
+	// long long - 8
+
+	/*bool x = true;
+	char a = '@';
+	int b = 42;
+	short c = 42;
+	long d = 42L;
+	long long e = 42LL;*/
+
+	// || - or
+	// ! - not
+	// && - and
+
+	/*if (c == d)
+	{
+		std::cout << "equals" << '\n';
+	}
+	else if (c != d)
+	{
+		std::cout << "not equals" << '\n';
+	}
+	else
+	{
+		std::cout << "will not work";
+	}*/
+	/////////////////////
+	///// CONSTANTS
+	//double v;
+
+	//std::cin >> v;
+
+	//constexpr double PI = 3.14;
+	////const double PI = v;
+
+	//std::cout << PI * 2;
+
+
+	//std::cout << '\n';
+	/////////////////////
+	//std::cout << "1. Open file" << '\n';
+	//std::cout << "2. Send file" << '\n';
+	//std::cout << "3. Delete file" << '\n';
+	//std::cout << "4. Exit" << '\n';
+
+	//int op;
+
+	//std::cout << "Enter operation -> ";
+	//std::cin >> op;
+
+	//constexpr int variant1 = 1;
+
+	//switch (op)
+	//{
+	//	case variant1: // if (op == 1)
+	//	{
+	//		std::cout << "File opened" << '\n';
+
+	//		break;
+	//	}
+
+	//	case 2: // if (op == 2)
+	//	{
+	//		std::cout << "File sent" << '\n';
+
+	//		break;
+	//	}
+
+	//	
+
+	//	case 3: // if (op == 3)
+	//	{
+	//		std::cout << "File deleted" << '\n';
+
+	//		break;
+	//	}
+	//	//case 3.28F: // will not work
+
+	//	case 4: // if (op == 4)
+	//	{
+	//		std::cout << "Program closed" << '\n';
+	//		
+	//		break;
+	//	}
+
+	//	default: // else
+	//	{
+	//		std::cout << "Incorrect command" << '\n';
+
+	//		break;
+	//	}
+	//}
+	////////////////
+	//char key;
+
+	//std::cin >> key;
+
+	//switch (key)
+	//{
+	//	case 87:
+	//	case 119:
+	//		std::cout << "Moving forward" << '\n';
+	//		break;
+	//	//case 65: // will not work
+	//	case 'A':
+	//	case 'a':
+	//		std::cout << "Moving left" << '\n';
+	//		break;
+
+	//	case 'S':
+	//	case 's':
+	//		std::cout << "Moving backward" << '\n';
+	//		break;
+	//	case 'D':
+	//	case 'd':
+	//		std::cout << "Moving right" << '\n';
+	//		break;
+	//}
+}
