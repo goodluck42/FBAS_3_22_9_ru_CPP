405ecfc184cf780698aa98596b97aab7f9f0a7cc
Alex <xrlogic1@gmail.com>
2023-05-14 17:12:27 +0400

    *

--- FileSystem/FileSystem.cpp
+++ FileSystem/FileSystem.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdio.h>
 
-
 const char* FILE_NAME = "persons.bin";
 
 struct Person
@@ -10,17 +9,71 @@ struct Person
 	int age;
 };
 
+size_t array_growth(size_t capacity, size_t length)
+{
+	return capacity * 2;
+}
+
+template<typename T> void reallocate_array(T*& data, size_t& length, size_t& capacity)
+{
+	if (length == capacity)
+	{
+		T* new_data = new T[capacity * 2];
+
+		for (size_t i = 0; i < length; ++i)
+		{
+			new_data[i] = data[i];
+		}
+
+		capacity *= 2;
+
+		delete[] data;
+
+		data = new_data;
+	}
+}
+
+template<typename T> void append(T*& data, T element, size_t& length, size_t& capacity)
+{
+	reallocate_array(data, length, capacity);
+	data[length++] = element;
+
+	//if (length < capacity)
+	//{
+	//	data[length++] = element;
+	//}
+	//else
+	//{
+	//	T* new_data = new T[array_growth(capacity, length)]; // capacity * 2;
+
+	//	for (size_t i = 0; i < length; ++i)
+	//	{
+	//		new_data[i] = data[i];
+	//	}
+	//	delete[] data;
+
+	//	new_data[length++] = element;
+
+	//	capacity *= 2;
+
+	//	data = new_data;
+	//}
+}
+
+template<typename T> T* create_array(size_t capacity)
+{
+	return new T[capacity];
+}
+
 bool write_Person(Person& person)
 {
 	FILE* file = nullptr;
 
-	fopen_s(&file, FILE_NAME, "a");
+	fopen_s(&file, FILE_NAME, "ab");
 
 	if (file != nullptr)
 	{
-		char* data = (char*)(&person); // MAGIC TRICK
-
-		fwrite(data, sizeof(char), sizeof(Person), file);
+		fwrite(&person, sizeof(Person), 1, file);
 
 		fclose(file);
 
@@ -30,37 +83,28 @@ bool write_Person(Person& person)
 	return false;
 }
 
-Person* read_Persons()
+Person* read_Persons(size_t& length, size_t& capacity)
 {
 	FILE* file = nullptr;
+	Person* persons = create_array<Person>(capacity);
 
-	Person* persons = new Person[6]{};
-
-	fopen_s(&file, FILE_NAME, "r");
+	fopen_s(&file, FILE_NAME, "rb");
 
 	if (file != nullptr)
 	{
-		char* buffer = new char[sizeof(Person)] {};
-
-		size_t i = 0;
+		Person* person = new Person;
 
 		while (true)
 		{
-			if (fread(buffer, sizeof(char), sizeof(Person), file) == 0)
+			if (fread(person, sizeof(Person), 1, file) == 0)
 			{
 				break;
 			}
 
-			Person* read_person = (Person*)(buffer);
-
-			strcpy_s(persons[i].name, sizeof(Person::name), read_person->name);
-
-			persons[i].age = read_person->age;
-
-			++i;
+			append(persons, *person, length, capacity);
 		}
 
-		delete[] buffer;
+		delete person;
 
 		fclose(file);
 	}
@@ -68,20 +112,51 @@ Person* read_Persons()
 	return persons;
 }
 
+void print_Person(Person& person)
+{
+	std::cout << "name = " << person.name << '\n';
+	std::cout << "age = " << person.age << '\n';
+}
 
 int main(int argc, char** argv)
 {
+	/*size_t capacity = 3;
+	size_t len = 0;
+
+	int* array = create_array<int>(capacity);
+
+	append(array, 10, len, capacity);
+	append(array, 42, len, capacity);
+	append(array, 13, len, capacity);
+	append(array, 44, len, capacity);
+	append(array, 11, len, capacity);
+	append(array, 12, len, capacity);
+	append(array, 19, len, capacity);
+
+	for (size_t i = 0; i < len; ++i)
+	{
+		std::cout << array[i] << '\n';
+	}
+
+	std::cout << "length = " << len << '\n';
+	std::cout << "capacity = " << capacity << '\n';
+
+
+	delete[] array;*/
+
 	//Person p1{ "StASS", 19 };
 
 	//write_Person(p1);
 
+	size_t length = 0;
+	size_t capacity = 3;
 
+	Person* persons = read_Persons(length, capacity);
 
-	Person* persons = read_Persons();
-
-	std::cout << persons[0].name << '\n';
-	std::cout << persons[1].name << '\n';
-	std::cout << persons[2].name << '\n';
+	for (size_t i = 0; i < length; ++i)
+	{
+		print_Person(persons[i]);
+	}
 
 	delete[] persons;
 
@@ -92,6 +167,13 @@ int main(int argc, char** argv)
 	// w+ - write & read
 	// r+ - read & write
 	// a+ - read & append
+	// b - binary
+	// wb
+	// rb
+	// ab
+	// wb+
+	// rb+
+	// ab+
 
 
 	//{
--- Unions/Unions.cpp
+++ Unions/Unions.cpp
@@ -1,6 +1,73 @@
 #include <iostream>
 
+union Response
+{
+	int code;
+	char* message;
+};
+
+union Char
+{
+	char symbol;
+	unsigned long code;
+};
+
+struct Time
+{
+	long h : 5;
+	long m : 6;
+	long s : 6;
+};
+
+void foo(double) {} // foo_double
+void foo(int) {} // foo_int
+
+void print_Char(union Char a_char, int variant)
+{
+	switch (variant)
+	{
+		case 1:
+			std::cout << a_char.code << '\n';
+			break;
+		case 2:
+			std::cout << a_char.symbol << '\n';
+	}
+}
+
+struct Sample
+{
+	char a[9];
+};
+
+union Test
+{
+	Sample a; // double 8 of 8
+	long b; 
+};
+
+
+union B
+{
+	double a;
+	int b;
+};
+
+struct A
+{
+	B a;
+	char c;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << sizeof(Time) << '\n';
+	std::cout << sizeof(Test) << '\n';
+	std::cout << sizeof(A) << '\n';
+
+	/*Char c{};
+
+	c.code = 66;
+
+	std::cout << c.symbol << '\n';
+	std::cout << c.code << '\n';*/
 }
